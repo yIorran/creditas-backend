f7274665cc1bb44b277870d24c4faaecc4679502
yIorran <iorran.gv@hotmail.com>
2024-12-16 19:27:09 -0300

    feat: unused imports

--- src/main/java/com/creditas/controller/mapper/LoanRequestMapper.java
+++ src/main/java/com/creditas/controller/mapper/LoanRequestMapper.java
@@ -5,8 +5,6 @@ import com.creditas.usecase.model.LoanSimulationUseCaseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface LoanRequestMapper {
 
